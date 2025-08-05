97960d937bf9675222c6c04d7ad04f1b26e44a9a
milan <milan@gmail.com>
2025-08-06 02:57:11 +0500

    Working on Assignment 2 part 2 finalizing

--- src/Ride.java
+++ src/Ride.java
@@ -1,13 +1,18 @@
 public class Ride implements RideInterface {
     private String name;
     private int duration;
+    private int thrillLevel;
     private Employee operator;
 
-    public Ride() {}
 
-    public Ride(String name, int duration, Employee operator) {
+    public Ride() {
+
+    }
+
+    public Ride(String name, int duration, int thrillLevel, Employee operator) {
         this.name = name;
         this.duration = duration;
+        this.thrillLevel = thrillLevel;
         this.operator = operator;
     }
     public String getName() {
@@ -30,5 +35,46 @@ public class Ride implements RideInterface {
     public void setOperator(Employee operator) { 
         this.operator = operator; 
     }
+
+    @Override
+    public void addVisitorToQueue(Visitor visit) {
+       
+    }
+
+    @Override
+    public void removeVisitorFromQueue() {
+        
+    }
+    @Override
+    public void printQueue() {
+        
+    }   
+    
+    @Override
+    public void runOneCycle() {
+
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visit) {
+
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visit) {
+        return false;
+    }
+
+    @Override
+    public int numberOfVisitors() {
+        return 0;
+    }
+
+    @Override
+    public void printRideHistory() {
+    }
+
+
+
 
 }
