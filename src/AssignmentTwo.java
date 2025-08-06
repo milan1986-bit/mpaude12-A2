644cdf84261fc9263554b7f04229ace6a2679947
milan <milan@gmail.com>
2025-08-06 14:04:12 +0500

    Working on Assignment 2 part 4 modifying the partFourA function according to new changes

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -30,6 +30,22 @@ public class AssignmentTwo {
     }
 
     public void partFourA() {
+
+        // Create Employee (name, gender, phone, id, department)
+        Employee operatorInCharge = new Employee("Bob", "Male", "0412 345 678", "E456", "Ride Attendant");
+
+        // Create Ride (name, duration, thrillLevel, operator)
+        Ride cosmicTwist = new Ride("Batwing Spaceshot", 130, 4, operatorInCharge); 
+
+        // Create 5 Visitors (name, gender, phone, tokenNumber, visitDetail)
+        Visitor v1 = new Visitor("Anna", "Female", "0421 111 111", "T010", "Excitement");
+        Visitor v2 = new Visitor("Ben", "Male", "0432 222 222", "T011", "Fun");
+        Visitor v3 = new Visitor("Cara", "Female", "0443 333 333", "T012", "Thrill");
+        Visitor v4 = new Visitor("Dan", "Male", "0454 444 444", "T013", "Challenge");
+        Visitor v5 = new Visitor("Ella", "Female", "0465 555 555", "T014", "Adventure");
+
+
+
     }
 
     public void partFourB() {
